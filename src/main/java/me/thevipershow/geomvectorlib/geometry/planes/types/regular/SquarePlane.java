40c6d57a424e1970a5da26f1f930c546a8ff7cce
TheViperShow <29604693+TheViperShow@users.noreply.github.com>
2020-04-30 15:19:43 +0200

    Irregular Solids introduction.

--- src/main/java/me/thevipershow/geomvectorlib/geometry/planes/types/regular/SquarePlane.java
+++ src/main/java/me/thevipershow/geomvectorlib/geometry/planes/types/regular/SquarePlane.java
@@ -11,12 +11,23 @@
  *
  */
 
-package me.thevipershow.geomvectorlib.geometry.planes.types;
+/*
+ *
+ *  * Software licensed by TheViperShow
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package me.thevipershow.geomvectorlib.geometry.planes.types.regular;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import me.thevipershow.geomvectorlib.geometry.planes.RegularPlaneShape;
 import me.thevipershow.geomvectorlib.pairs.DoublePair;
 import org.jetbrains.annotations.NotNull;
--- src/main/java/me/thevipershow/geomvectorlib/geometry/solids/types/platonic/SphereSolid.java
+++ src/main/java/me/thevipershow/geomvectorlib/geometry/solids/types/platonic/SphereSolid.java
@@ -11,11 +11,24 @@
  *
  */
 
-package me.thevipershow.geomvectorlib.geometry.solids.types;
+/*
+ *
+ *  * Software licensed by TheViperShow
+ *  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *  * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *  * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *  * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *  * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+package me.thevipershow.geomvectorlib.geometry.solids.types.platonic;
 
 import java.util.ArrayList;
 import java.util.List;
-import me.thevipershow.geomvectorlib.geometry.planes.types.CirclePlane;
+import me.thevipershow.geomvectorlib.geometry.planes.types.regular.CirclePlane;
 import me.thevipershow.geomvectorlib.geometry.solids.RegularSphericalShape;
 import me.thevipershow.geomvectorlib.pairs.DoublePair;
 import me.thevipershow.geomvectorlib.triples.DoubleTriple;
